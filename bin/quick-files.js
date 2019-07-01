a785e94b2faa383b0e57b76f73b8b01015b345f7
Sam Smith <sam@mintcanary.com>
2019-07-01 16:36:27 +0100

    update dependancies

--- bin/quick-files.js
+++ bin/quick-files.js
@@ -11,3 +11,11 @@ copyfiles(["node_modules/@whiteprint/components/dist/components.css", dest + "/c
 copyfiles(["node_modules/@whiteprint/components/dist/components.js", dest + "/js"], true, function (err) {
     if (err) console.error(err);
 });
+
+copyfiles(["node_modules/@whiteprint/placeholders/placeholders.css", dest + "/css"], true, function (err) {
+    if (err) console.error(err);
+});
+
+copyfiles(["node_modules/@whiteprint/placeholders/placeholders.js", dest + "/js"], true, function (err) {
+    if (err) console.error(err);
+});
