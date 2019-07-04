ea7c81f11187e27e8f63bfad0665f0c702413a53
Sam Smith <sam@mintcanary.com>
2019-07-04 16:17:36 +0100

    add layouts

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 
-gulp.task('prototype:js', function() {
-  return gulp.src(['../@whiteprint/components/dist/components.js', '../@whiteprint/placeholders/placeholders.js'])
-    .pipe(concat('prototype.js'))
+gulp.task('prototype:css', function() {
+  return gulp.src([
+    '../@whiteprint/base/index.css',
+    '../@whiteprint/components/dist/components.css',
+    '../@whiteprint/layouts/dist/index.css',
+    '../@whiteprint/placeholders/placeholders.css'
+  ])
+    .pipe(concat('prototype.css'))
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('prototype:css', function() {
-  return gulp.src(['../@whiteprint/components/dist/components.css', '../@whiteprint/placeholders/placeholders.css'])
-    .pipe(concat('prototype.css'))
+gulp.task('prototype:js', function() {
+  return gulp.src([
+    '../@whiteprint/components/dist/components.js',
+    '../@whiteprint/placeholders/placeholders.js'
+  ])
+    .pipe(concat('prototype.js'))
     .pipe(gulp.dest('./'));
 });
