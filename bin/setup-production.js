eb93c4fc63cb42c043b259823e466b065f89822b
Sam Smith <sam@mintcanary.com>
2019-07-08 15:33:10 +0100

    add js

--- bin/setup-production.js
+++ bin/setup-production.js
@@ -19,3 +19,7 @@ copyfiles(["./node_modules/whiteprint/templates/styles.css", dest + "/src/css"],
 copyfiles(["./node_modules/@whiteprint/components/lib/*/[^_]*.css", dest + "/src/css/components"], true, function (err) {
     if (err) console.error(err);
 });
+
+copyfiles(["./node_modules/@whiteprint/components/dist/components.js", dest + "/js"], true, function (err) {
+    if (err) console.error(err);
+});
